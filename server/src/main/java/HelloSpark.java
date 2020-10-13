6b0742db22f345796d0e312f27d3d92898f0db9d
Matthew Rutter <rutter.matt@ymail.com>
2020-10-12 21:07:55 -0400

    added additional comments and formatting for instruction

--- server/src/main/java/HelloSpark.java
+++ server/src/main/java/HelloSpark.java
@@ -1,7 +1,16 @@
+// Basic showcase of java spark framework use from preexisting examples online
+// Author: Matthew Rutter
+// Date: 10/12/2020
+
 import static spark.Spark.*;
 
 public class HelloSpark {
+
     public static void main(String[] args) {
+
+        // Use basic get request to respond to user with funny response
         get("/", (req, res) -> "This isn't Node! This is Spark! Java has nothing to do with Javascript! Did your bootcamp teach you anything?");
+
     }
+
 }
